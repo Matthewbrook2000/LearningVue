62c13f7e326438717c2c2adc06dcab512c761de1
unknown <matthewbrook2000@gmail.com>
2020-06-12 19:12:35 +0100

    first day done

--- firstproj/src/App.vue
+++ firstproj/src/App.vue
@@ -1,23 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="main">
+    <Nav />
+    <a>Type your name: </a>
+    <input type="text" v-model="name"/>
+    <h1>hello {{ name }}</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
 
 export default {
   name: 'App',
+  data: function(){
+    return {
+      name: ''
+    }
+  },
   components: {
+    Nav,
     HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
+#main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ firstproj/src/components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <ul>
+        <li><a href="bleh">home</a></li>   
+        <li><a href="bleh">about</a></li>  
+       </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
