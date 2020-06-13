2dcf774dfe05f6c0b189c07ab1f0011ebce5972c
unknown <matthewbrook2000@gmail.com>
2020-06-13 15:48:41 +0100

    Learnt alot, learn button nav pages

--- firstproj/src/App.vue
+++ firstproj/src/App.vue
@@ -1,10 +1,35 @@
 <template>
   <div id="main">
-    <Nav />
+    <Nav />  <!--Uses a component-->
     <a>Type your name: </a>
     <input type="text" v-model="name"/>
-    <h1>hello {{ name }}</h1>
+    <h1>hello {{ name + ' youre looking great'}}</h1> <!--moustaches can contain javascript which is cool-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--App 1, hover over text-->
+    <div id="hover">
+      <span v-bind:title="message">
+        Hover your mouse here
+      </span>
+    </div>
+    <!--App 2, if statement-->
+    <div id="ifstatement">
+        <span v-if="seen">
+          You can see this cause an if statement
+        </span>
+    </div>
+    <!--App 3, printing an array using for loop-->
+    <div id="todo">
+      <ol>
+        <li v-for="todo in todos" v-bind:key="todo">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
+    <!--App  4, reverse a message-->
+    <div id="revmsg">
+      <p>{{ revMessage }}</p>
+      <button v-on:click="reverseMsg">Reverse Message</button>
+    </div>
   </div>
 </template>
 
@@ -16,7 +41,19 @@ export default {
   name: 'App',
   data: function(){
     return {
-      name: ''
+      name: '',
+      message: 'Thanks for hovering',
+      revMessage: 'Watch me flip',
+      seen: true,
+      todos: [
+        { text: 'finish this vue app' },
+        { text: 'Make a cool website' }
+      ]
+    }
+  },
+  methods: {
+    reverseMsg: function () {
+      this.revMessage = this.revMessage.split('').reverse().join('')
     }
   },
   components: {
--- firstproj/src/components/Nav.vue
+++ firstproj/src/components/Nav.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
+  <div id="navbar">
       <ul>
-        <li><a href="bleh">home</a></li>   
-        <li><a href="bleh">about</a></li>  
+        <li><a href="home" v-onclick="">Home</a></li>  
+        <li><a href="app1">App 1</a></li>   
+        <li><a href="app2">App 2</a></li>  
+        <li><a href="app3">App 3</a></li>
+        <li><a href="app4">App 4</a></li>    
        </ul>
   </div>
 </template>
@@ -16,5 +19,30 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: white;
+}
 
+li {
+  display: inline;
+  float: left;
+
+}
+
+li a {
+  display: block;
+  padding: 14px 16px;
+  text-align: center;
+  background-color: white; 
+  text-decoration: none;
+  color: black;
+}
+
+li a:hover {
+  background-color: #f0f0f0;
+}
 </style>
